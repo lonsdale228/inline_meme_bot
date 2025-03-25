fb62ca176190be7e7751fe5b4a1a0e8da570bdb0
bebus <v.gerashchenkoua@gmail.com>
2025-03-25 19:04:45 +0200

    added guide

--- handlers/start.py
+++ handlers/start.py
@@ -10,6 +10,19 @@ from database.utils import get_users, add_user_to_group
 
 router = Router()
 
+guide = """
+To use this bot, firstly you need drop here any meme you wanna save!
+
+Then you can access to this meme with: @inlinusbot meme <keywords>
+Bot will give you hints.
+All meme are sorted to groups:
+
+gif - GIF
+video - Videos
+st - Stickers
+audio - Audios
+"""
+
 @router.message(Command("start"), StateFilter(None))
 async def start(message: Message, state: FSMContext):
     await state.clear()
@@ -21,4 +34,5 @@ async def start(message: Message, state: FSMContext):
         await add_user_to_group(str(message.from_user.id), referral)
         await message.answer(f"You have been added to group!")
     else:
-        await message.answer(f"Hello!")
+        await message.answer(f"Hello! \n" +
+                             guide)
