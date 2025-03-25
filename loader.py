3fc258e7e0e11d3799e21d00d99c54024aacdf74
bebus <v.gerashchenkoua@gmail.com>
2025-03-25 19:38:33 +0200

    some refactoring

--- loader.py
+++ loader.py
@@ -1,7 +1,14 @@
 import logging
 import os
 
-from aiogram import Bot
+from handlers.inline_mode import router as r1
+from handlers.start import router as r3
+from handlers.saving_images import router as image_router
+from handlers.user import router as user_router
+from handlers.add_to_groups import router as add_to_groups_router
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger = logging.getLogger(__name__)
@@ -10,3 +17,6 @@ logger.setLevel(logging.ERROR)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(BOT_TOKEN)
+
+dp = Dispatcher()
+dp.include_routers(r1,r3,image_router,user_router,add_to_groups_router)
--- main.py
+++ main.py
@@ -1,30 +1,14 @@
 import asyncio
-import os
 
-from aiogram import Dispatcher, Bot
-
-from handlers.inline_mode import router as r1
-from handlers.start import router as r3
-from handlers.saving_images import router as image_router
-from handlers.user import router as user_router
-from handlers.add_to_groups import router as add_to_groups_router
 from database.connection import sessionmanager
 from database.models import Base
-import loader
-
-
-
-dp = Dispatcher()
-dp.include_routers(r1,r3,image_router,user_router,add_to_groups_router)
-
-
-
+from loader import dp, bot
 
 
 async def main():
     async with sessionmanager.connect() as connection:
         await connection.run_sync(Base.metadata.create_all)
-    await dp.start_polling(loader.bot)
+    await dp.start_polling(bot)
 
 
 
