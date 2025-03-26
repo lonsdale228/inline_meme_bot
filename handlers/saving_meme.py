c16b70a3e04b15b8bb4bb539dcf2abc9b0be3959
bebus <v.gerashchenkoua@gmail.com>
2025-03-26 19:43:14 +0200

    some refactoring

--- handlers/saving_meme.py
+++ handlers/saving_meme.py
@@ -9,70 +9,9 @@ from handlers.add_to_groups import AddToGroup, add_meme_to_group
 
 router = Router()
 
-# class NamePicture(StatesGroup):
-#     choosing_sticker_key_words = State()
-#
-# class NameGif(StatesGroup):
-#     choosing_gif_key_words = State()
-
 class NameMeme(StatesGroup):
     naming_meme = State()
 
-# @router.message(F.sticker, StateFilter(None))
-# async def sticker_handler(message: Message, state: FSMContext):
-#
-#     sticker_id = message.sticker.file_id
-#
-#     await state.update_data(sticker_id=sticker_id)
-#
-#     await message.answer("Enter sticker name/keywords")
-#
-#     await state.set_state(NamePicture.choosing_sticker_key_words)
-#
-# @router.message(F.text, StateFilter(NamePicture.choosing_sticker_key_words))
-# async def get_sticker_name(message: Message, state: FSMContext):
-#
-#     data = await state.get_data()
-#     sticker_name = message.text.strip()
-#
-#     is_added = await add_meme(title=sticker_name, description="", mime_type="sticker", file_id=data['sticker_id'], is_private=False,
-#                               user_id=str(message.from_user.id))
-#     if is_added:
-#         await message.answer("Added!")
-#     else:
-#         await message.answer("Already exists!")
-#
-#     await state.clear()
-#
-#
-#
-#
-# @router.message(F.animation, StateFilter(None))
-# async def gif_handler(message: Message, state: FSMContext):
-#     gif_id = message.animation.file_id
-#
-#     await state.update_data(gif_id=gif_id)
-#
-#     await message.answer("Enter gif name/keywords")
-#
-#     await state.set_state(NameGif.choosing_gif_key_words)
-#
-#
-# @router.message(F.text, StateFilter(NameGif.choosing_gif_key_words))
-# async def get_gif_name(message: Message, state: FSMContext):
-#
-#     data = await state.get_data()
-#     gif_name = message.text.strip()
-#
-#     meme_id = await add_meme(title=gif_name, description="", mime_type="gif", file_id=data['gif_id'], is_private=False,
-#                               user_id=str(message.from_user.id))
-#     if meme_id != -1:
-#         await message.answer(f"Added! {meme_id}")
-#         await add_meme_to_group(message, state, meme_id)
-#     else:
-#         await message.answer("Already exists!")
-#
-#     await state.set_state(AddToGroup.add_meme_to_group)
 
 @router.message(F.photo, StateFilter(None))
 @router.message(F.audio, StateFilter(None))
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 
 from aiogram import Dispatcher, Bot
 
-from handlers.inline_mode import router as r1
-from handlers.start import router as r3
-from handlers.saving_images import router as image_router
+from handlers.inline_mode import router as inline_router
+from handlers.start import router as start_router
+from handlers.saving_meme import router as saving_meme_router
 from handlers.user import router as user_router
 from handlers.add_to_groups import router as add_to_groups_router
 from database.connection import sessionmanager
@@ -15,7 +15,7 @@ import loader
 
 
 dp = Dispatcher()
-dp.include_routers(r1,r3,image_router,user_router,add_to_groups_router)
+dp.include_routers(inline_router,start_router,saving_meme_router,user_router,add_to_groups_router)
 
 
 
